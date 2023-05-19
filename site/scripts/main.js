89e5690de612721fbcc2d9e5741ed8eea663afdf
vickygamer997 <122418123+vickygamer997@users.noreply.github.com>
2023-05-19 14:54:13 +0000

    Uploaded Files

---
+++ site/scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/logo.png") {
+    myImage.setAttribute("src", "images/logo-red.png");
+  } else {
+    myImage.setAttribute("src", "images/logo.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Welcome to V Facts, ${myName}`;
+    }
+  }
+  
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome back, ${storedName}`;
+  }
+myButton.onclick = () => {
+    setUserName();
+  };  
